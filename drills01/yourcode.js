81f0725b96bb9c09f908b46be161cc3362f3d429
Christine Le <christinele@csu.fullerton.edu>
2019-09-23 19:03:21 -0700

    drills 01 attempt. Still need to test rest of functions after 2

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,10 +1,27 @@
 
-function sumArray(){
-	
-}
+function sumArray(sumArray){
+  var total = 0;
+    for (var i = 0; i < sumArray.length; i++){
+      total += sumArray[i];
+    }
+  return total;
+  }
 
-function fitWithinVal(){
 
+function fitWithinVal(sumArray){
+  console.log(sumArray)
+//go through the loop/values
+//add value to sum
+//check if sum is too big
+//subtract value if it is
+  var total = 0;
+  for (var i = 1; i <= sumArray.length; i++){
+    for (var j = 0; j <= 30; j++){
+      total += sumArray[j];
+      console.log(total)
+    }
+    return total;
+  }
 }
 
 function getAllNamesShorterThan(){
